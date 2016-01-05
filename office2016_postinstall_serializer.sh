d8f5952745e1a25991135e4eeda53590510e15df
smashism <hellomodtitan@gmail.com>
2016-01-05 14:51:23 -0600

    Create office2016_postinstall_serializer.sh
    designed to deploy the office 2016 volume installer, app updater pkgs, and then run the VL serializer (new to volume copies of office as of 15.17, available through MS VLSC).

---
+++ office2016_postinstall_serializer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Determine working directory
+
+install_dir=`dirname $0`
+
+# Run primary VL installer for suite
+
+/usr/sbin/installer -dumplog -verbose -pkg $install_dir/"Microsoft_Office_2016_Volume_Installer.pkg" -target "$3"
+
+# Run Individual Update pkgs not included in VL installer
+
+/usr/sbin/installer -dumplog -verbose -pkg $install_dir/"Microsoft_Word_15.17.1_Updater.pkg" -target "$3"
+/usr/sbin/installer -dumplog -verbose -pkg $install_dir/"Microsoft_Outlook_15.17.1_Updater.pkg" -target "$3"
+
+# Run VL Serializer pkg
+
+/usr/sbin/installer -dumplog -verbose -pkg $install_dir/"Microsoft_Office_2016_VL_Serializer.pkg"
+
+exit 0
