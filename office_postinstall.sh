8a083832e35c5463c15fe10ef2d6f1f50da1f57f
smashism <hellomodtitan@gmail.com>
2016-07-14 11:53:19 -0500

    Update office_postinstall.sh
    added choices.plist for selective app install

--- office_postinstall.sh
+++ office_postinstall.sh
@@ -4,16 +4,15 @@
 
 install_dir=$(dirname "$0")
 
-# Run primary app installers
+# Run primary installer with choices file
 
-/usr/sbin/installer -dumplog -verbose -pkg "$install_dir/Microsoft_Word_2016_15.23.0_160611_Installer.pkg" -target "$3"
-/usr/sbin/installer -dumplog -verbose -pkg "$install_dir/Microsoft_Excel_2016_15.23.0_160611_Installer.pkg" -target "$3"
-/usr/sbin/installer -dumplog -verbose -pkg "$install_dir/Microsoft_PowerPoint_2016_15.23.0_160611_Installer.pkg" -target "$3"
+/usr/sbin/installer -dumplog -verbose -pkg "$install_dir/Microsoft_Office_2016_15.23.0_160611_Volume_Installer.pkg" -target "$3" -applyChoiceChangesXML "$install_dir/choices.plist"
 
 # Run individual Update pkgs
 
-/usr/sbin/installer -dumplog -verbose -pkg "$install_dir/Microsoft_Word_15.23.1_160617_Updater.pkg" -target "$3"
-/usr/sbin/installer -dumplog -verbose -pkg "$install_dir/Microsoft_Excel_15.23.1_160617_Updater.pkg" -target "$3"
+/usr/sbin/installer -dumplog -verbose -pkg "$install_dir/Microsoft_Word_15.24.0_160709_Updater.pkg" -target "$3"
+/usr/sbin/installer -dumplog -verbose -pkg "$install_dir/Microsoft_Excel_15.24.0_160709_Updater.pkg" -target "$3"
+/usr/sbin/installer -dumplog -verbose -pkg "$install_dir/Microsoft_PowerPoint_15.24.0_160709_Updater.pkg" -target "$3"
 
 # Why not Zoidberg?
 
